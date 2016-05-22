50843ed83ebfac543e6e4db3eee3e5dde5c0317b
Michael Krapf <Michael Krapf@Michael-Laptop.home>
2016-05-22 15:30:03 +0200

    ...

--- Wettbuero/src/WettDBManager.java
+++ Wettbuero/src/WettDBManager.java
@@ -4,10 +4,51 @@
  *
  */
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class WettDBManager 
 {
-	public static void main(String[] args) 
+	private Connection conn;
+	
+	public WettDBManager()
 	{
 		
+	}
+	
+	public ArrayList<Account> getAccounts()
+	{
+		
+	}
+	
+	public Wette getWette(Wette wette)
+	{
+		
+	}
+	
+	public Wettobjekt getWettobjekt(Wettobjekt wettobjekt)
+	{
+		
+	}
+	
+	public void setAccount(Account account)
+	{
+		
+	}
+	
+	public void setWette(Wette wette)
+	{
+		
+	}
+	
+	public void setWettobjekte(Wettobjekt wettobjekt)
+	{
+		
+	}
+	
+	public void close() throws SQLException
+	{
+		conn.close();
 	}
 }
--- Wettbuero/src/WettManager.java
+++ Wettbuero/src/WettManager.java
@@ -4,7 +4,25 @@
  *
  */
 
+import java.util.ArrayList;
+
 public class WettManager 
 {
+	private WettDBManager wettDBManager;
+	public ArrayList<Account> accountListe;
 	
+	public void wetteSetzen(Wette wette)
+	{
+		
+	}
+	
+	public void accountsErstellen(Account account)
+	{
+		
+	}
+	
+	public void wettObjektSetzen(Wettobjekt wettobjekt)
+	{
+		
+	}
 }
