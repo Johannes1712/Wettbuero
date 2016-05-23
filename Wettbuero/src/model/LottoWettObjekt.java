7e0adddcc4fdf9ea0712a3542911cce0e536c726
User <User@JOHANNES>
2016-05-23 08:14:19 +0200

    JH

---
+++ Wettbuero/src/model/LottoWettObjekt.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.Observable;
+
+public class LottoWettObjekt extends Wettobjekt_Observeable {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
