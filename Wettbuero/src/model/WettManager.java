0ee23d3a48661fb5bf5529f627d90af42136e561
Michael Krapf <Michael Krapf@Michael-Laptop.home>
2016-05-29 18:06:54 +0200

    gemerged

--- Wettbuero/src/model/WettManager.java
+++ Wettbuero/src/model/WettManager.java
@@ -5,7 +5,25 @@ package model;
  *
  */
 
+import java.util.ArrayList;
+
 public class WettManager 
 {
+	private WettDBManager wettDBManager;
+	public ArrayList<Account> accountListe;
 	
+	public void wetteSetzen(Wette wette)
+	{
+		
+	}
+	
+	public void accountsErstellen(Account account)
+	{
+		
+	}
+	
+	public void wettObjektSetzen(Wettobjekt wettobjekt)
+	{
+		
+	}
 }
