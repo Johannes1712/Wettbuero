27f4547750bb2a34f659aa4f6344ed2a07464e0a
User <User@JOHANNES>
2016-05-30 08:45:54 +0200

    JH

--- Wettbuero/src/model/Wettobjekt_Observeable.java
+++ Wettbuero/src/model/Wettobjekt_Observeable.java
@@ -2,49 +2,54 @@ package model;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Observable;
 import java.util.Observer;
 import java.util.Timer;
 import java.util.TimerTask;
 
-abstract class Wettobjekt_Observeable extends Observable{
+abstract class Wettobjekt_Observeable extends TimerTask{
 	protected int id;
 	protected Date begin;
 	protected Date end;
-	protected ArrayList<Wette> wetten;
-	public ArrayList<Observer> observer;
+	protected ArrayList<Wette> wettenliste;
+	public ArrayList<Observer> observerliste;
 
-	public Wettobjekt_Observeable() {
+	public Wettobjekt_Observeable(Date begin, Date end) {
+		
+		wettenliste= new ArrayList<Wette>();
+		observerliste= new ArrayList<Observer>();
+		this.begin=begin;
+		this.end=end;
+		
 		Timer t = WettTimerSingleton.getInstance();
 		t.schedule(this, end);
 		t.schedule(this, begin);
 	}
 
 	public void wetten (Wette wette) {
-		
+		wettenliste.add(wette);
 	}
 
-	public void run () {
+	/*public void run() {
 		if(Zeit < end){
 		  start();
 		} else{
 			end();
 		}
-	}
+	}*/
 
 	abstract void start();
 
 	abstract void end();
 
 	public void alsObserveableMarkieren (Observer observer){
-		this.addObserver(observer);
+		observerliste.add(observer);
 	}
 
-	public void alleObserverUpdateSenden () {
+	/*public void alleObserverUpdateSenden () {
 		if(this.hasChanged()){
 			this.notifyObservers(this);
 			this.clearChanged();
 		}
-	}
+	}*/
 
 }
--- Wettbuero/src/model/ZahlenWettObjekt.java
+++ Wettbuero/src/model/ZahlenWettObjekt.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.HashMap;
 import java.util.Observable;
 
 public class ZahlenWettObjekt extends Wettobjekt_Observeable {
@@ -20,7 +21,57 @@ public class ZahlenWettObjekt extends Wettobjekt_Observeable {
 
 	@Override
 	void end() {
+		this.ziehung()
+		this.gewinnErmittlung();
+	}
+	
+	public  void ziehung(HashMap<Wette> tipps){
 		
+		System.out.println("Alle Wettbeteiligten mit Betrag und Tipp:");
+		for(Benutzer px: tipps.keySet()){
+			
+			System.out.println(px + "€ Tipp:"+ tipps.get(px));
+		}
+		
+		
+		System.out.println("======================================================");
+		System.out.println("Die Ziehung beginnt:");
+		
+		int zufallszahl= (int) (Math.random()*100)+1;
+		
+		gewinnErmittlung(zufallszahl,tipps);
+		
+		
+		System.out.println("Die Ziehung ist beendet:");
+		System.out.println("Die Zahl lautet:"+ zufallszahl);
+		System.out.println("======================================================");
+			
+	}
+	
+	public void gewinnErmittlung(int zufallszahl, HashMap<Benutzer,Integer> tipps){
+		
+		for(Benutzer px: tipps.keySet()){
+						
+			int tipp= tipps.get(px);
+			if(tipp>95 && zufallszahl<4){
+				int zahl=100+zufallszahl;
+				if((zahl-tipp)<=2){px.setBetrag(px.getBetrag()*5); continue;}
+				if((zahl-tipp)<=5){px.setBetrag(px.getBetrag()*2); continue;}
+			}
+			if(zufallszahl>95 && tipp<4){
+				int zahl=100+tipp;
+				if((zahl-zufallszahl)<=2){px.setBetrag(px.getBetrag()*5); continue;}
+				if((zahl-zufallszahl)<=5){px.setBetrag(px.getBetrag()*2); continue;}
+			}
+			
+			if(zufallszahl<10)
+			if(tipp < (zufallszahl % 100) +2  && tipp > (zufallszahl % 100)-2) {
+				px.setBetrag(px.getBetrag()*5); continue;
+			}
+			if(tipp < (zufallszahl % 100)+5 && tipp > (zufallszahl % 100)-5){
+				px.setBetrag(px.getBetrag()*2); continue;
+			}			
+		}
 	}
 
 }
