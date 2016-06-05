15e3efd634182423c8b679e3e1ba39b7c90ba2e0
User <User@JOHANNES.home>
2016-06-05 13:10:18 +0200

    JH

--- Wettbuero/src/model/Wettobjekt_Observeable.java
+++ Wettbuero/src/model/Wettobjekt_Observeable.java
@@ -1,8 +1,12 @@
 package model;
 
 import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Observer;
+import java.util.TimeZone;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,6 +16,7 @@ abstract class Wettobjekt_Observeable extends TimerTask{
 	protected boolean gestartet;
 	protected Date begin;
 	protected Date end;
+	protected Date date;
 	protected ArrayList<Wette> wettenliste;
 	public ArrayList<Observer> observerliste;
 
@@ -23,6 +28,7 @@ abstract class Wettobjekt_Observeable extends TimerTask{
 		this.end=end;
 		this.beschreibung=beschreibung;
 		
+		date = new Date(0);
 		Timer t = WettTimerSingleton.getInstance();
 		t.schedule(this, end);
 		t.schedule(this, begin);
@@ -35,15 +41,15 @@ abstract class Wettobjekt_Observeable extends TimerTask{
 		}
 	}
 
-	/*public void run() {
-		if(Zeit < end){
+	public void run() {
+		if( date.before(end) ){
 		  start();
 		} else{
 			end();
-			//alleObserverUpdateSenden();
+			alleObserverUpdateSenden();
 		}
-	}*/
-
+	}
+	
 	public void start(){
 		gestartet=true;
 	}
